4aee598e1ab6fc66dff2ad63e03dc964e3fc2bdf
Mikhail Nosov <manosov1984@gmail.com>
2025-05-07 20:39:17 +0300

    feat: add simple tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,48 @@
-// Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({ a: 1, b: 2, action: Action.Add });
+    expect(result).toBe(3);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({ a: 3, b: 1, action: Action.Subtract });
+    expect(result).toBe(2);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({ a: 2, b: 2, action: Action.Multiply });
+    expect(result).toBe(4);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({ a: 9, b: 3, action: Action.Divide });
+    expect(result).toBe(3);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({
+      a: 2,
+      b: 2,
+      action: Action.Exponentiate,
+    });
+    expect(result).toBe(4);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const result = simpleCalculator({ a: 5, b: 3, action: '?' });
+    expect(result).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const invalidFirstArg = { a: '1', b: 1, action: Action.Add };
+    expect(simpleCalculator(invalidFirstArg)).toBeNull();
+
+    const invalidSecondArg = { a: 1, b: '1', action: Action.Subtract };
+    expect(simpleCalculator(invalidSecondArg)).toBeNull();
+
+    const invalidBothArgs = { a: '1', b: '1', action: Action.Multiply };
+    expect(simpleCalculator(invalidBothArgs)).toBeNull();
   });
 });
